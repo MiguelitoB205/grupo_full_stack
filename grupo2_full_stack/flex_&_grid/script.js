03f6b7d85e589bcb0e65c3f556d0b1fbd5bc1726
Miguel Buitrago <miguelcatholic@gmail.com>
2024-06-09 15:59:23 -0500

    subiendo nuevas platnillas de HTML para video

--- grupo2_full_stack/flex_&_grid/script.js
+++ grupo2_full_stack/flex_&_grid/script.js
@@ -4,3 +4,37 @@ function mostrarValores(){
 
     document.getElementById("formulario").innerHTML = texto
 }
+
+/*function miArray(){
+    const albumes = [{
+        nombre: "Cerca de tí", artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+    },
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+},
+{
+    nombre: "Cerca de tí",img:'/img/cerca_de_ti.jpeg', artista: "Jesús Adrián Romero", anio: 1999, genero: "Alabanza a Dios"
+}]
+    document.getElementById("caja").innerHTML
+}*/
