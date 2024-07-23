caf91384a6d14bfca8a58ebe6884d701aa00ffb9
Miguel Buitrago <miguelcatholic@gmail.com>
2024-07-23 17:42:49 -0500

    subiendo nuevo form
    Co-authored-by: dlegarda1 <dlegarda1@users.noreply.github.com>
    Co-authored-by: jj05-0514 <jj05-0514@users.noreply.github.com>

--- grupo2_full_stack/frontend/programa.js
+++ grupo2_full_stack/frontend/programa.js
@@ -1,76 +1,29 @@
 // crear nuevo registro
-/*async function crearUsuario(){
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const correo = document.getElementById('email').value;
-    const contrasena = document.getElementById('password').value;
+
+
+async function crearRegistro(){
+    const nomb=document.getElementById('nombre').value;
+    console.log(nomb);
+    const apel = document.getElementById('apellido').value;
+    const correo=document.getElementById('email').value;
+    const contrasena=document.getElementById('password').value;
     try{
-        const respuesta = await axios.post('/crearUsuario',{
-            nombre: nombre,
-            apellido: apellido,
+        const respuesta= await axios.post('/crearUsuario',{
+            nombre:nomb,
+            apellido:apel,
             email:correo,
-            password:contrasena
-            })
-            document.getElementById('respuesta').innerHTML=respuesta.data;
-            console.log(respuesta.data);
-            if(respuesta.data){
-                document.getElementById('boton').style.display="none";
-                document.getElementById('respuesta').innerHTML="Gracias por registrarte";
-            }
-    } catch(error){
+            password:contrasena })
+        document.getElementById('respuesta').innerHTML=respuesta.data;
+        if(respuesta.data){
+            window.location.href = '/usuarioCreado';
+        }
+        console.log(respuesta.data);
+    }
+    catch(error){
         console.log(error);
     }
 }
-document.getElementById('boton').addEventListener('click', crearUsuario)
 
-// listar usuarios
+document.getElementById('boton').addEventListener('click',crearRegistro);
 
-async function cargarDatos() {
-    try{
-        const respuesta= await axios.get('/usuarios')        
-    if (respuesta.data) {           
-      const userList = document.getElementById('listaUsuarios');
-      userList.innerHTML = ''; 
-        respuesta.data.forEach(user => {
-        const option = document.createElement('option');
-        option.value = user.id;
-        option.textContent = `${user.nombre}& ${user.apellido} & ${user.correo}`;
-        userList.appendChild(option);
-      });
-    }
-}
-catch(error){
-    console.log(error); 
-  }      
-}
-cargarDatos()
-
-async function confirmarPago(){
-    
-    const numeroCuenta = document.getElementById('numeroCuenta')
-
-    try{
-        const confirmacionPago = await axios.post('/pagarAlbum',{
-            numeroCuenta: numeroCuenta
-        })
-        document.getElementById('confirmacionPago').innerHTML=confirmacionPago.data
-    } catch(error){
-        
-    }
-
-
-
-}*/
-
-function validarRegistro(){
-    let nombre = document.forms["registro"]["nombre"].value;
-    let apellido = document.forms["registro"]["apellido"].value;
-    let email = document.forms["registro"]["email"].value;
-    let password = document.forms["registro"]["password"].value;
-    let celular = document.forms["registro"]["celular"].value;
-    if(nombre=="" && apellido=="" && email=="" &&password=="" && celular==""){
-        alert("Falta completar el formulario")
-        return false
-    } 
-}
 
--- grupo2_full_stack/index.js
+++ grupo2_full_stack/index.js
@@ -4,31 +4,16 @@ const path = require('path')
 const app = express()
 const port = 3300
 
+
+app.use(express.static(__dirname + '/frontend'));
 app.set('view engine', 'html')
 app.get('/', (req, res) => {
   res.send('Hola mundo!')
 })
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-const validacionFormulario = (req,res,next)=>{
-  const nombre = req.body.nombre
-  const apellido = req.body.apellido
-  const correo = req.body.email;
-  const contrasena=req.body.password;
-  if(!nombre){
-    res.send('Falta el nombre')
-  } else if(!apellido){
-    res.send("Falta el apellido")
-  }
-   else if(!correo){
-    res.send('Faltó el correo')
-  } else if(!contrasena){
-    res.send('Faltó la contraseña')
-  }
-  else{
-    next();
-  }
-}
+
+
 /******************** */
 // midleware de rutas
 app.use((req, res, next)=>{
@@ -36,23 +21,38 @@ app.use((req, res, next)=>{
   next();
 })
 
+const validacionFormulario = (req, res, next) => {
+  const nombre = req.body.nombre;  
+  const correo = req.body.email;
+  const contrasena = req.body.password;
+  if (!nombre) {
+    res.send('falto el nombre');
+  } else if (!correo) {
+    res.send('falto el correo');
+  } else if (!contrasena) {
+    res.send('falto la contraseña');
+  }
+  else {
+    next();
+  }
+}
+
 app.get('/home', (req, res) => {
   res.sendFile(__dirname +'/frontend/practica.html')
 })
-app.get('/registro', (req, res) => {
-res.sendFile(__dirname +'/frontend/formulario.html')
-})
+
 app.get('/videos', (req, res) => {
 res.sendFile(__dirname +'/frontend/videos.html')
 })
-app.get('formas_pago', (req, res)=>{
+app.get('/formas_pago', (req, res)=>{
   res.sendFile(__dirname + '/frontend/formaPagos.html')
 })
-
-app.get('/saludo/:nombre', (req,res)=>{
-const nombre = req.params.nombre;
-res.send(`Hola ${nombre}!`)
+app.get('/confirmacion_pago',(req,res)=>{
+  res.sendFile(__dirname + '/frontend/confirmacionPago.html')
 })
+
+
+
 
 
 // conexión a la base de datos
@@ -81,52 +81,27 @@ const userSchema = new Schema({
 const User = mongoose.model('User', userSchema)
 
 //Crear nuevo usuario
-app.post('/crearUsuario', validacionFormulario, async(req,res)=>{
-  const nomb = req.body.nombre
-  const apell = req.body.apellido
-  const correo = req.body.email
-  const contrasena = req.body.password
-  const user = new User({nombre: nomb, 
-     apellido: apell,
-     email: correo, 
-     password: contrasena});
-  try{
-  await user.save()
-  res.send('Usuario creado')
-  console.log('Usuario creado');
-}
-catch(err){
-  res.send('Error al crear usuario', err)
-}}
-)
-//Obtener todos los usuarios
-
-app.get('/usuarios',async(req, res)=>{
-  try{
-  const users = await User.find();
-  res.send(users)
-  console.log(users);
+app.post('/crearUsuario', validacionFormulario, async (req, res) => {
+  const nombre = req.body.nombre;
+  const correo = req.body.email;
+  const contrasena = req.body.password;
+  const user = new User({ nombre: nombre, correo: correo, contrasena: contrasena });
+  try {
+    await user.save()
+    res.send('Usuario creado');
+    console.log('Usuario creado');
   }
-  catch(err){
-    res.send('Error al obtener usuarios', err)
+  catch (err) {
+    res.send('Error al crear usuario', err);
   }
 })
 
-app.post('/usuario/nombre', async (req, res)=>{
-  const nomb = req.body.nombre;
-  console.log(nomb);
-  try{
-  const user = await User.findOne({nombre:nomb})
-  if(!user){
-    res.send('Usuario no encontrado')
-  } else{
-    res.send(user)
-    console.log(user);
-  }
-} catch(err){
-  res.send('Error al obtener usuario', err);
-}
-})
+//*************************************** */
+//      Ruta para usuario creado
+app.get('/usuarioCreado', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/confirmacionPago.html'));
+});
+
 
 
 app.listen(port, () => {
